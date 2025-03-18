47aa882e3f804fa3cf36339c9c712deadff76a6c
omri93rajuan <omri93rajuan@gmail.com>
2025-03-18 14:25:12 +0200

    fix: Remove spec file

--- users-microservice/src/express/features/manager.ts
+++ users-microservice/src/express/features/manager.ts
@@ -8,26 +8,34 @@ export class UsersManager {
     };
 
     static getById = async (userId: string): Promise<UserDocument> => {
-        return UsersModel.findById(userId).orFail(new DocumentNotFoundError(userId)).lean().exec();
+        return UsersModel.findById(userId)
+            .orFail(new DocumentNotFoundError(userId))
+            .lean()
+            .exec();
     };
 
     static getByGenesisId = async (genesisId: string): Promise<UserDocument> => {
-        return UsersModel.findOne({ genesisId }).orFail(new DocumentNotFoundError(genesisId)).lean().exec();
+        return UsersModel.findOne({ genesisId })
+            .orFail(new DocumentNotFoundError(genesisId))
+            .lean()
+            .exec();
     };
 
     static createOne = async (user: User): Promise<UserDocument> => {
-        const existingUser = await UsersModel.findOne({ genesisId: user.genesisId }).lean().exec();
-        if (existingUser) {
-            throw new Error('User already exists');
-        }
         return UsersModel.create(user);
     };
 
     static updateOne = async (userId: string, update: Partial<User>): Promise<UserDocument> => {
-        return UsersModel.findByIdAndUpdate(userId, update, { new: true }).orFail(new DocumentNotFoundError(userId)).lean().exec();
+        return UsersModel.findByIdAndUpdate(userId, update, { new: true })
+            .orFail(new DocumentNotFoundError(userId))
+            .lean()
+            .exec();
     };
 
     static deleteOne = async (userId: string): Promise<UserDocument> => {
-        return UsersModel.findByIdAndDelete(userId).orFail(new DocumentNotFoundError(userId)).lean().exec();
+        return UsersModel.findByIdAndDelete(userId)
+            .orFail(new DocumentNotFoundError(userId))
+            .lean()
+            .exec();
     };
 }
