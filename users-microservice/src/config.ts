70968a1e12bfc34486ab65dbb9a3fc297168ab8f
omri93rajuan <omri93rajuan@gmail.com>
2025-03-20 15:34:09 +0200

    fix: update config to support prod and dev

--- users-microservice/src/config.ts
+++ users-microservice/src/config.ts
@@ -1,12 +1,15 @@
-import 'dotenv/config';
 import env from 'env-var';
+import 'dotenv/config';
 
 export const config = {
     service: {
-        port: env.get('PORT').default(3000).required().asPortNumber(),
+        port: env.get('PORT').required().asPortNumber(),
     },
     mongo: {
-        uri: env.get('MONGO_URI').default('mongodb://localhost').asString(),
-        usersCollectionName: env.get('USERS_COLLECTION_NAME').default('users').required().asString(),
+        uri:
+            env.get('NODE_ENV').asString() === 'production'
+                ? env.get('MONGO_URI_PROD').required().asString()
+                : env.get('MONGO_URI_DEV').required().asString(),
+        systemsCollectionName: env.get('SYSTEMS_COLLECTION_NAME').required().asString(),
     },
 };
--- users-microservice/src/index.ts
+++ users-microservice/src/index.ts
@@ -1,19 +1,24 @@
-/* v8 ignore start */
 import mongoose from 'mongoose';
 import { config } from './config.js';
 import { Server } from './express/server.js';
 import { logger } from './utils/logger/index.js';
+import env from 'env-var';
 
 const { mongo, service } = config;
 
 const initializeMongo = async () => {
-    logger.info('Connecting to Mongo...');
+    const environment = env.get('NODE_ENV').default('development').asString();
 
-    await mongoose.connect(mongo.uri);
+    logger.info(`Connecting to Mongo... [Environment: ${environment}]`);
 
-    logger.info('Mongo connection established');
+    try {
+        await mongoose.connect(mongo.uri);
+        logger.info(`Mongo connection established [${environment}]`);
+    } catch (error) {
+        logger.error(`Failed to connect to Mongo`);
+        process.exit(1);
+    }
 };
-
 const main = async () => {
     await initializeMongo();
 
