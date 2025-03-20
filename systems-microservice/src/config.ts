fc268a02e52ef3c9521bbdc9a3f8d2bcc70d7f93
omri93rajuan <omri93rajuan@gmail.com>
2025-03-20 15:31:26 +0200

    fix: update config to support prod and dev

--- systems-microservice/src/config.ts
+++ systems-microservice/src/config.ts
@@ -1,5 +1,4 @@
 import env from 'env-var';
-
 import 'dotenv/config';
 
 export const config = {
@@ -7,7 +6,10 @@ export const config = {
         port: env.get('PORT').required().asPortNumber(),
     },
     mongo: {
-        uri: env.get('MONGO_URI').required().asString(),
-        systemsCollectionName: env.get('SYSYEMS_COLLECTION_NAME').required().asString(),
+        uri:
+            env.get('NODE_ENV').asString() === 'production'
+                ? env.get('MONGO_URI_PROD').required().asString()
+                : env.get('MONGO_URI_DEV').required().asString(),
+        systemsCollectionName: env.get('SYSTEMS_COLLECTION_NAME').required().asString(),
     },
 };
